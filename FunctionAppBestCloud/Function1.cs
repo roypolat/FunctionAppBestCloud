9f186cc3d32c8df7d89f19f607c28d2020ec0b8f
Roy Polat <roy.polat@bilgiedu.net>
2022-02-04 18:51:25 +0300

    Proje dosyası ekle.

---
+++ FunctionAppBestCloud/Function1.cs
@@ -0,0 +1,86 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+namespace FunctionApp2
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "temperature")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+
+            string city = req.Query["city"];
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            city = city ?? data?.city;
+            string uri = String.Format("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=xml&appid=4238ec8727d226cd414e33119add4e34");
+            XmlTextReader reader = new XmlTextReader(uri);
+            List<string> mylist = new List<string>();
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+
+
+
+                        while (reader.MoveToNextAttribute()) // Read the attributes.
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+
+                        Console.Write("");
+                        Console.WriteLine("");
+                        mylist.Add(reader.Name);
+                        mylist.Add(reader.Value);
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+
+
+
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("" + reader.Name);
+
+
+                        Console.WriteLine("");
+
+                        break;
+                }
+
+            }
+
+            string jsonData = @"{'FirstName':'Roy','LastName':'Polat'}";
+            
+
+            string responseMessage = string.IsNullOrEmpty(city)
+                ? "This HTTP triggered function executed successfully. Pass a city in the query string or in the request body for a personalized response."
+                : $" {jsonData}  \xA {city} = {mylist[5]}";
+
+
+
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
